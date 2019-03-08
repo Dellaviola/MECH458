f19b77f4444a3198f65cec7499f5ef7a042daae3
pizzalich <40552225+pizzalich@users.noreply.github.com>
2019-03-08 13:35:10 -0800

    added acceleration and peek ahead.

--- stepper.c
+++ stepper.c
@@ -21,6 +21,8 @@
 #include "config.h"
 #include "blinky.h"
 
+#define TURN_180 100
+
 
 //Stepper Globals ------- moved into header, replaced with struct
 // volatile char step[4] = {0x30, 0x06, 0x28, 0x05};
@@ -37,7 +39,36 @@ void Stepper_Setup()
 
 }
 
-void Stepper(int rotation, int direction)
+int NumSteps(int target, int current)
+{
+	int steps = (target - current);
+	if (steps >= 0)
+	{
+		//if there are a positive number of steps > 180; subtract 180 and rotate the other way
+		steps = (steps > TURN_180) ? (-1) * (steps - TURN_180) : (steps);
+	}
+	else
+	{
+		//if there are a negative number of steps < 180; add 180 and rotate the other way
+		steps = (steps < (-1 * TURN_180)) ? (steps) : (-1) * (steps + TURN_180);
+	}
+	return steps;
+}
+
+void Rotate(int target, int current, int next)
+{
+	//Steps and dirrection to position
+	steps = NumSteps(target, current);
+	//Steps and dirrection to next pos
+	nextSteps = NumSteps(next, target);
+	stepper._dirrection = (steps >= 0) ? CW : CCW;
+	bool cont = (steps * nextSteps >= 0) TRUE : FALSE;
+
+	//setpper can not take -ve numbers of steps
+	Stepper(abs(steps), stepper._dirrection, cont);
+}
+
+void Stepper(int rotation, int direction, bool cont)
 {
 	//PORTC = 0xAA;
 	//(stepper_param) arg;
@@ -49,8 +80,16 @@ void Stepper(int rotation, int direction)
 		//PORTC = (direction == CW) ? (PORTA = step[stepper._stepNum]) : (PORTA = step[3 - stepper._stepNum]);
 		PORTC = (direction == CW) ? (step[stepper._stepNum]) : (step[3 - stepper._stepNum]);
 		PORTA = (direction == CW) ? (step[stepper._stepNum]) : (step[3 - stepper._stepNum]);
-        stepper._stepNum = (stepper._stepNum == 3) ? (stepper._stepNum = 0) : (stepper._stepNum + 1) ;
-        Delay_Create(20);
+		stepper._stepNum = (stepper._stepNum == 3) ? (stepper._stepNum = 0) : (stepper._stepNum + 1);
+		Delay_Create(stepper._delay);
 		PORTC = 0x00;
-    }
+
+		//Simple accell / decel blcok
+		if ((i > 5) && (stepper._delay > 6))
+			stepper._delay -= 2;
+		if (((roation - i) <= 5) && (stepper._delay < 20))
+			stepper._delay += 2;
+	}
+	//if not continuing in the same dirrections reset the delay
+	stepper._delay = (cont) ? stepper._delay : 20;
 }
